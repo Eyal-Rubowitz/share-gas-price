835770b6f1c75c465fb09303b0582de37cc238fb
Eyal Rubowitz <eyalr26@gmail.com>
2023-08-26 16:54:30 +0300

    layout files added to new & update/:id stations folders

---
+++ app/stations/new/layout.js
@@ -0,0 +1,13 @@
+export const metadata = {
+    titl: 'New station',
+    description: 'New station page to create new station with all its info'
+}
+
+export default function NewStationLayout({ children }) {
+
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
