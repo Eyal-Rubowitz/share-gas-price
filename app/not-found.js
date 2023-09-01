c402e6767bd275114a9bbbed5e3c543867a3139d
Eyal Rubowitz <eyalr26@gmail.com>
2023-09-01 18:07:28 +0300

    Removal of unnecessary committed line.

--- app/not-found.js
+++ app/not-found.js
@@ -1,11 +1,7 @@
 import Link from 'next/link';
-import React from 'react';
-// import {notFound} from "next/navigation"
 
 export default function NotFound() {
   return (
-    <React.StrictMode>
-
     <main className="text-center">
         <h2 className="text-3xl">
             404 Page Not Found!
@@ -13,6 +9,5 @@ export default function NotFound() {
         <p>There was a problem the page you looking for is not found.</p>
         <Link href='/'>Go Back to the main page</Link>
     </main> 
-    </React.StrictMode>
   )
 }
