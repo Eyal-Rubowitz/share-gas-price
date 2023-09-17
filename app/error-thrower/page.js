273bf8c0c3e9e138209b37ee55452e71f6f04643
Eyal Rubowitz <eyalr26@gmail.com>
2023-09-17 14:59:34 +0300

    try a smarter error thrower

--- app/error-thrower/page.js
+++ app/error-thrower/page.js
@@ -3,7 +3,10 @@
 import { useState } from 'react';
 const ErrorThrower = () => {
     const [clicked, setClicked] = useState(false);
-    return clicked ? <div>{JSON.parse('{this throws an error')}</div> : <button onClick={() => setClicked(true)}>Throw error</button>;
+    if (clicked)
+        throw new Error('cool error');
+    else
+        return <button onClick={() => setClicked(true)}>Throw error</button>;
 };
 
 export default ErrorThrower;
