c503f4ebef137ed6fc5e99b2c4c23d6d7c172ba5
Eyal Rubowitz <eyalr26@gmail.com>
2023-09-17 14:54:46 +0300

    try a smart error thrower

--- app/error-thrower/page.js
+++ app/error-thrower/page.js
@@ -1,5 +1,9 @@
+'use client'
+
+import { useState } from 'react';
 const ErrorThrower = () => {
-    return <div>{JSON.parse(fetch('non-existing-url'))}</div>
+    const [clicked, setClicked] = useState(false);
+    return clicked ? <div>{JSON.parse('{this throws an error')}</div> : <button onClick={() => setClicked(true)}>Throw error</button>;
 };
 
 export default ErrorThrower;
